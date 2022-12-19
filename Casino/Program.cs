9cb733ac2b9311041ebfb543ef21d693e801f787
Pritam <averagestudent70@gmail.com>
2022-12-19 17:48:08 +0530

    Added option for early exit.

--- Casino/Program.cs
+++ Casino/Program.cs
@@ -8,6 +8,7 @@
 			double odds = 0.75;
 			
 			Player player = new Player() { Cash = 100, Name = "The Player" };
+			int previousBalance = player.Cash;
 
 			Console.WriteLine($"Welcome to the casino. The odds are {odds}.");
 
@@ -16,9 +17,12 @@
 				player.WriteMyInfo();
 				
 				Console.Write("How much do you want to bet: ");
-				string howMuch = Console.ReadLine();
+				string input = Console.ReadLine();
 
-				if (int.TryParse(howMuch, out int amount))
+				if (input == "")
+					break;
+
+				if (int.TryParse(input, out int amount))
 				{
 					int pot = player.GiveCash(amount) * 2;  // the player makes a double-or-nothing bet every round.
 					if (pot > 0)
@@ -34,13 +38,19 @@
 				}
 				else
 				{
-					Console.WriteLine("Please enter a valid amount.");
+					Console.WriteLine("Please enter a valid amount (press enter to exit.)");
 				}
 
 				Console.WriteLine();
 			}
+			int currentBalance = player.Cash;
 
-			Console.WriteLine("Thank you for playing. Better luck next time.");
+			Console.WriteLine();
+			Console.WriteLine("Previous Balance: " + previousBalance);
+			Console.WriteLine("Current Balance: " + currentBalance);
+			Console.WriteLine("You " + (currentBalance >= previousBalance ? "won: " : "lost: ") 
+								+ Math.Abs(previousBalance - currentBalance));
+			Console.WriteLine("Thank you for playing!");
 		}
 	}
 }
